0d07e8ec8a306843c1104a40b8f36424683b88ae
CHOUDHURY SAI BAIBHAB MOHAPATRA <saimohapatra86@gmail.com>
2026-08-30 18:46:11 +0530

    Delete week_3 directory

--- week_3/L1_3.java
+++
@@ -1,9 +0,0 @@
-class L1_3{
-    static void printNumbersUpToN(int n){
-        for(int i=1;i<=n;i++)
-            System.out.println(i);
-    }
-    public static void main(String[] args){
-        printNumbersUpToN(5);
-    }
-}
